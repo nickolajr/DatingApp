1d4eec45180e7156f7b415e7034463b35c00c53f
Tec <Tec@SKAB2-PC1>
2023-08-24 13:18:06 +0200

    Add project files.

---
+++ DatingApp/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace DatingApp.Models
+{
+    public class Account
+    {
+        public string accountname { get; set; }
+        public string password { get; set; }
+        public int tlf { get; set; }
+        public DateTime date { get; set; }
+        public string username { get; set; }
+        
+
+    }
+
+}
